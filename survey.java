eab7b76ede63823edbef589e2da27cd3a2610cd7
Leo <leonardommfontoura@gmail.com>
2022-03-04 17:30:36 -0300

    exemplo de como usar o scan para receber inputs do usuarios

--- survey.java
+++ survey.java
@@ -1,11 +1,17 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class survey {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in).useLocale(Locale.ENGLISH);
         System.out.println("Whats your name?");
         String name = scan.nextLine();
         System.out.println("Hello " + name);
+        System.out.println("Quanto dinheiro quer pagar no cafe?");
+        double valor = scan.nextDouble();
+        System.out.println("Seu nome e " + name + " voce pagou " + valor + " no cafe");
+
+        scan.close();
     }
 }
 
